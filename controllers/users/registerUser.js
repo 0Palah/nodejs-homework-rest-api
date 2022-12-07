c69d848498b80f6f62692f018656b32176d53ddc
Oleg Palashynskyi <palah@ukr.net>
2022-12-07 22:56:17 +0200

    user1

--- controllers/users/registerUser.js
+++ controllers/users/registerUser.js
@@ -1,6 +1,14 @@
+const User = require("../../models/users");
+
 async function registerUser(req, res) {
-  console.log(req.body);
-  res.send();
+  const { password, email, subscription } = req.body;
+
+  const result = await User.create({ password, email, subscription });
+  res.status(201).json({
+    email: result.email,
+    password: result.password,
+    subscription: result.subscription,
+  });
 }
 
 module.exports = registerUser;
