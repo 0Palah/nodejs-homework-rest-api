9441dcfa31121d198694e62dd58a553f771c1740
Oleg Palashynskyi <palah@ukr.net>
2022-12-10 11:58:33 +0200

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,6 +1,14 @@
+const { createError } = require("../helpers/createError");
+
 async function authenticate(req, res, next) {
   try {
     const { authorization } = req.headers;
+
+    const [bearer, token] = authorization.split(" ");
+
+    if (bearer !== "Bearer") {
+      throw createError({ status: 401, message: "Unauthorized" });
+    }
   } catch (error) {
     console.error(error);
     next(error);
