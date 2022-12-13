d8769c17a7dce973b0683576d95350dba3822ffa
Oleg Palashynskyi <palah@ukr.net>
2022-12-13 18:38:07 +0200

    Update getAll.js

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,20 +2,27 @@ const Contact = require("../../models/contacts/index");
 
 async function getAll(req, res) {
   const { _id } = req.user;
-  const { page = 1, limit = 20, favorite = true } = req.query;
+  // const { page = 1, limit = 20, favorite = true } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
 
   const skip = (page - 1) * limit;
 
-  console.log(req.query);
+  const searchParams = {
+    owner: _id,
+  };
 
-  const result = await Contact.find(
-    { owner: _id, favorite },
-    "-createdAt -updatedAt",
-    {
-      skip,
-      limit,
-    }
-  ).populate("owner", "email");
+  if (favorite === "true") {
+    searchParams.favorite = favorite;
+  }
+
+  if (favorite === "false") {
+    searchParams.favorite = favorite;
+  }
+
+  const result = await Contact.find(searchParams, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
 
   res.json(result);
 }
