6713903448a822f78bd35e36ce4c1d8e063d5036
Oleg Palashynskyi <106759871+0Palah@users.noreply.github.com>
2022-12-31 10:31:06 +0200

    Update logoutUser.js

--- controllers/users/logoutUser.js
+++ controllers/users/logoutUser.js
@@ -3,6 +3,9 @@ const User = require("../../models/users");
 async function logoutUser(req, res) {
   const { _id } = req.user;
 
+  // const { refreshToken } = req.cookies;
+  // console.log(refreshToken);
+
   await User.updateOne({ _id }, { token: "" });
   res.status(204).json({
     message: "No Content",
